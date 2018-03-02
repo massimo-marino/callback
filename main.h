9e656477f179df557bf223a969e72add504829cc
Massimo Marino <massimo.marino@ieee.org>
2018-03-02 15:37:16 +0100

    removed some unneeded semicolons in macros

--- main.h
+++ main.h
@@ -15,11 +15,11 @@
 #define DEBUG_LOG
 #ifdef DEBUG_LOG
 // macros L and LL give the same result
-#define L std::cout << "[" << __FILE__ << ":" << __LINE__ << ": " << __func__ << "]: ";
-#define LL [](const std::string& function) {std::cout << "[" << __FILE__ << ":" << __LINE__ << ": " << function << "]: ";}(__func__);
+#define L std::cout << "[" << __FILE__ << ":" << __LINE__ << ": " << __func__ << "]: "
+#define LL [](const std::string& function) {std::cout << "[" << __FILE__ << ":" << __LINE__ << ": " << function << "]: ";}(__func__)
 // macros LMSG and LLMSG give the same result
-#define LMSG(msg) std::cout << "[" << __FILE__ << ":" << __LINE__ << ": " << __func__ << "]: " << msg << std::endl;
-#define LLMSG(msg) [](const std::string& function, const std::string& s) {std::cout << "[" << __FILE__ << ":" << __LINE__ << ": " << function << "]: "<< s  << std::endl;}(__func__, msg);
+#define LMSG(msg) std::cout << "[" << __FILE__ << ":" << __LINE__ << ": " << __func__ << "]: " << msg << std::endl
+#define LLMSG(msg) [](const std::string& function, const std::string& s) {std::cout << "[" << __FILE__ << ":" << __LINE__ << ": " << function << "]: "<< s  << std::endl;}(__func__, msg)
 #else
 #define L
 #define LL
